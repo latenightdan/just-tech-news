518186d8f5c96c991e1978377a52a4a52d1b62b4
daniel natale <dnatale667@gmail.com>
2021-11-15 12:51:23 -0500

    not done

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 //turn on routes
 app.use(routes);
 
-sequelize.sync({ force: false}).then(() =>{
+sequelize.sync({ force: false }).then(() =>{
     app.listen(PORT, () => console.log('listening'));
 
 });
