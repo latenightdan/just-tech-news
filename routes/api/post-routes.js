3a587c2c350e0ffdbb66b46d4b0645f24ffd2ad4
daniel natale <dnatale667@gmail.com>
2021-11-19 13:15:27 -0500

    wi

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 // const { post } = require('.');
+
 const { Post, User } = require('../../models');
+
 
 router.get('/', (req, res) => {
     console.log('=================');
