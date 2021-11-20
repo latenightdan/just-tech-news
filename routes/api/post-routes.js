fa1339f26c7a4dd98a6cff64b704ab6cec3f884e
daniel natale <dnatale667@gmail.com>
2021-11-20 18:20:24 -0500

    lesson 1 done

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 // const { post } = require('.');
 const sequelize = require('../../config/connection')
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 
 router.get('/', (req, res) => {
@@ -29,17 +29,34 @@ router.get('/', (req, res) => {
 });
 
 Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
-    order: [['created_at', 'DESC']],
+    attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at'
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+
     include: [
+        {
+            model: Comment,
+            attributes: [
+                'id',
+                'comment_text',
+                'post_id', 'user_id',
+                'created_at'
+            ],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
         {
             model: User,
             attributes: ['username']
         }
     ]
-}
-
-);
+});
 
 router.get('/:id', (req, res) => {
     Post.findOne({
@@ -56,8 +73,22 @@ router.get('/:id', (req, res) => {
             ],
         include: [
             {
-                model: User,
-                attributes: ['username']
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'created_at'],
+              include: {
+                  model: Post,
+                  attributes: ['title']
+              }  
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
             }
         ]
     })
@@ -88,11 +119,11 @@ router.post('/', (req, res) => {
 });
 router.put('/upvote', (req, res) => {
     Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 });
 router.put('/:id', (req, res) => {
     Post.update(
